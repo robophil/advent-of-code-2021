ac4b3baf470ef10a4c1d3ac915d6248e2d966191
phil <6032018+robophil@users.noreply.github.com>
2023-07-28 19:21:24 +0200

    feat: solve day 1 part 1

--- src/questions/day1.rs
+++ src/questions/day1.rs
@@ -3,9 +3,43 @@ use crate::solutions::{Solution, SolutionResult};
 #[derive(Default)]
 pub struct Question;
 
+struct Count {
+    last_value: i32,
+    count: i32,
+    first_run: bool,
+}
 impl Question {
     fn solve(&self, input: &Vec<String>) -> SolutionResult {
-        Ok("1".to_string())
+        let result = input.iter().fold(
+            Count {
+                last_value: 0,
+                count: 0,
+                first_run: true,
+            },
+            |acc, f| {
+                let num = f.parse::<i32>().unwrap();
+                if acc.first_run {
+                    Count {
+                        last_value: num,
+                        count: acc.count,
+                        first_run: false,
+                    }
+                } else if num > acc.last_value {
+                    Count {
+                        last_value: num,
+                        count: acc.count + 1,
+                        first_run: false,
+                    }
+                } else {
+                    Count {
+                        last_value: num,
+                        count: acc.count,
+                        first_run: false,
+                    }
+                }
+            },
+        );
+        Ok(result.count.to_string())
     }
 }
 
